f5dccfd30ac171cd50902c16bf01b3fb3a824a21
brendarg03 <106269087+brendarg03@users.noreply.github.com>
2024-10-26 10:48:46 -0600

    Cambio 1 Ramirez
    Confirmación Ok Ramirez 1

---
+++ proyectoISRamirez/src/proyectoisramirez/ProyectoISRamirez.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoisramirez;
+
+/**
+ *
+ * @author brend
+ */
+public class ProyectoISRamirez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba de respositorio 1 IS Ramriez 26/10/2024");
+        System.out.println("Fase de desarrollo - Repositorio en la nube");
+    }
+    
+}
